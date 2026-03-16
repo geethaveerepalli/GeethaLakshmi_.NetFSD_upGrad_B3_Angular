09fe445ccfc45e20439dc927b4e4d858ee1450a1
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-03-16 18:03:24 +0530

    Add files via upload

---
+++ week5/Day1/Program1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ConsoleApp39
+{
+    internal class Program
+    {
+        class BankAccount
+        {
+            private int _accountNumber;
+            private double _balance;
+
+            public int AccountNumber
+            {
+                get { return _accountNumber; }
+                set { _accountNumber = value;}
+            }
+
+            public double Balance
+            {
+                get { return _balance; }
+            }
+
+            public void Deposit(double amount)
+            {
+                if(amount > 0)
+                {
+                    _balance += amount;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid deposit amount");
+                }
+            }
+             public void Withdraw(double amount)
+            {
+                if(amount <= 0)
+                {
+                    Console.WriteLine("Invalid withdraw amount");
+                }
+                else if (amount > _balance)
+                {
+                    Console.WriteLine("Insufficient Balance");
+                }
+                else
+                {
+                    _balance -= amount;
+                    Console.WriteLine($"Withdraw Amount : {amount}");
+                 
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            BankAccount acc = new BankAccount();
+            Console.Write("Enter Account Number: ");
+            acc.AccountNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Deposit Amount: ");
+            double depositAmount = double.Parse(Console.ReadLine());
+
+            acc.Deposit(depositAmount);
+            Console.Write("Enter Withdraw Amount: ");
+            double withdrawAmount = double.Parse(Console.ReadLine());
+
+            acc.Withdraw(withdrawAmount);
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Final Balance = {acc.Balance}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
