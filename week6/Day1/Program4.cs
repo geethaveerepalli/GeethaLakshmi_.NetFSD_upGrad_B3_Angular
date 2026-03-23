ae4939e2dc35d9e638789c5ccd53fef412679afb
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-03-23 18:06:15 +0530

    Add files via upload

---
+++ week6/Day1/Program4.cs
@@ -0,0 +1,38 @@
+﻿using System;using System.IO;using System.Threading.Tasks;using System.Net.Http;namespace ConsoleApp8{    class Program    {        static async Task Main()
+        {
+            Console.WriteLine("Order Processing Started...\n");
+
+            await ProcessOrder();
+
+            Console.WriteLine("\nOrder Processing Completed.");
+        }
+
+        static async Task ProcessOrder()
+        {
+            await VerifyPaymentAsync();
+            await CheckInventoryAsync();
+            await ConfirmOrderAsync();
+        }
+
+        static async Task VerifyPaymentAsync()
+        {
+            Console.WriteLine("Payment Verification Started...");
+            await Task.Delay(2000);
+            Console.WriteLine("Payment Verified.");
+        }
+
+        static async Task CheckInventoryAsync()
+        {
+            Console.WriteLine("Inventory Check Started...");
+            await Task.Delay(1500);
+            Console.WriteLine("Inventory Available.");
+        }
+
+        static async Task ConfirmOrderAsync()
+        {
+            Console.WriteLine("Order Confirmation Started...");
+            await Task.Delay(1000);
+            Console.WriteLine("Order Confirmed.");
+        }
+    }
+}
