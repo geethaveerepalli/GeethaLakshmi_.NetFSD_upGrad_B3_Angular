1c5ec9ff1255090872a5b4ddbf490eee8c164409
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-03-24 18:28:59 +0530

    Delete week6/Day1 directory

--- week6/Day1/Program1.cs
+++
@@ -1,22 +0,0 @@
-﻿using System;using System.IO;using System.Threading.Tasks;using System.Net.Http;namespace ConsoleApp8{    class Program    {        static async Task Main(string[] args)        {
-            Console.WriteLine("Application Started...");
-
-            
-            Task t1 = WriteLogAsync("User logged in");
-            Task t2 = WriteLogAsync("File uploaded");
-            Task t3 = WriteLogAsync("Data saved");
-
-            Console.WriteLine("Main thread is still running...");
-            await Task.WhenAll(t1, t2, t3);
-
-            Console.WriteLine("All logs written successfully.");        }
-
-        static async Task WriteLogAsync(string message)
-        {
-            Console.WriteLine($"Start writing log: {message}");
-            await Task.Delay(2000);
-
-            Console.WriteLine($"Log written: {message}");
-        }
-    }
-}
