f5c4561078ea8c8ad956b197937817e72ad3a15c
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-03-31 18:17:32 +0530

    Add files via upload

---
+++ week7/Day2/Problem2/CalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("Calculator")]
+    public class CalculatorController : Controller
+    {
+        [HttpGet("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+
+            ViewData["Result"] = result;
+
+            return View("Index"); 
+        }
+    }
+}
---
+++ week7/Day2/Problem4/FeedbackController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("feedback")]
+    public class FeedbackController : Controller
+    {
+        
+        [HttpGet("form")]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+       
+        [HttpPost("form")]
+        public IActionResult Form(Feedback feedback)
+        {
+            if (feedback.Rating >= 4)
+            {
+                ViewData["Message"] = $"Thank You {feedback.Name} for your feedback!";
+            }
+            else
+            {
+                ViewData["Message"] = $"We will improve, {feedback.Name}. Thanks for your feedback!";
+            }
+
+            return View();
+        }
+    }
+}
