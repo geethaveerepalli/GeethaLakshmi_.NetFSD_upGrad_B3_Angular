ae9ea041f38fc4db22c56d1c7ca2337697be7b1e
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-04-02 18:04:59 +0530

    Add files via upload

---
+++ week7/Day4/HandsOn_2/ContactService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using WebApplication5.Models;
+using WebApplication5.Repository;
+namespace WebApplication5.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly IContactRepository _repo;
+
+        public ContactService(IContactRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public List<ContactInfo> GetAllContacts()
+        {
+            return _repo.GetAllContacts();
+        }
+
+        public ContactInfo GetContactById(int id)
+        {
+            return _repo.GetContactById(id);
+        }
+
+        public void AddContact(ContactInfo contact)
+        {
+            _repo.AddContact(contact);
+        }
+
+        public void UpdateContact(ContactInfo contact)
+        {
+            _repo.UpdateContact(contact);
+        }
+
+        public void DeleteContact(int id)
+        {
+            _repo.DeleteContact(id);
+        }
+    }
+}
