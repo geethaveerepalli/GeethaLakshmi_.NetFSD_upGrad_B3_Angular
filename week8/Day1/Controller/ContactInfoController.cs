bc88e5c1e3d1ba0c94e0d611839e86ff8d1a8a2a
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-04-07 16:00:58 +0530

    Add files via upload

---
+++ week8/Day1/Controller/ContactInfoController.cs
@@ -0,0 +1,71 @@
+﻿using ContactApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication7.Models;
+
+namespace ContactApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService _service;
+
+        public ContactController(IContactService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var contacts = _service.GetContacts();
+            return View(contacts);
+        }
+
+     
+        public IActionResult Details(int id)
+        {
+            var contact = _service.GetContact(id);
+            return View(contact);
+        }
+
+       
+        public IActionResult Create()
+        {
+            ViewBag.Companies = new SelectList(_service.GetCompanies(), "CompanyId", "CompanyName");
+            ViewBag.Departments = new SelectList(_service.GetDepartments(), "DepartmentId", "DepartmentName");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ContactInfo contact)
+        {
+            _service.Create(contact); 
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            var contact = _service.GetContact(id);
+
+            ViewBag.Companies = new SelectList(_service.GetCompanies(), "CompanyId", "CompanyName");
+            ViewBag.Departments = new SelectList(_service.GetDepartments(), "DepartmentId", "DepartmentName");
+
+            return View(contact);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ContactInfo contact)
+        {
+            _service.Update(contact);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _service.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ week8/Day1/Services/ContactService.cs
@@ -0,0 +1,50 @@
+﻿using ContactApp.Repositories;
+using WebApplication7.Models;
+
+namespace ContactApp.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly IContactRepository _repo;
+
+        public ContactService(IContactRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IEnumerable<ContactInfo> GetContacts()
+        {
+            return _repo.GetAll();
+        }
+
+        public ContactInfo GetContact(int id)
+        {
+            return _repo.GetById(id);
+        }
+
+
+        public void Create(ContactInfo contact)
+        {
+            _repo.Add(contact);
+        }
+
+        public void Update(ContactInfo contact)
+        {
+            _repo.Update(contact);
+        }
+
+        public void Delete(int id)
+        {
+            _repo.Delete(id);
+        }
+        public IEnumerable<Company> GetCompanies()
+        {
+            return _repo.GetCompanies();
+        }
+
+        public IEnumerable<Department> GetDepartments()
+        {
+            return _repo.GetDepartments();
+        }
+    }
+}
