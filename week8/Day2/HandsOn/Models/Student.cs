8e774f44d9a3fbb49aa83c6baf68b57a248d74d3
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-04-07 17:22:44 +0530

    Add files via upload

---
+++ week8/Day2/HandsOn/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication9.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ week8/Day2/HandsOn/Repository/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication9.Models;
+
+namespace WebApplication9.Repositories
+{
+    public interface IStudentRepository
+    {
+        IEnumerable<Student> GetStudentsWithCourse();
+        IEnumerable<Course> GetCoursesWithStudents();
+        IEnumerable<Course> GetAllCourses();
+        void AddStudent(Student student);
+    }
+}
