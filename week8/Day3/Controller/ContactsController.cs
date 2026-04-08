259735aae8299fc852c3b3a772e8987044f17bc8
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-04-08 16:13:05 +0530

    Add files via upload

---
+++ week8/Day3/Controller/ContactsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication12.Models;
+using WebApplication12.Repository;
+
+namespace WebApplication12.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactRepository _repo;
+        public ContactsController(IContactRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _repo.GetAllAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var contact = await _repo.GetByIdAsync(id);
+            if (contact == null)
+            {
+                return NotFound("Contact Not Found");
+
+            }
+            else
+            {
+                return Ok(contact);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] ContactInfo contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                var created = await _repo.AddAsync(contact);
+                return CreatedAtAction(nameof(GetById), new { id = created.ContactId }, created);
+
+            }
+
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] ContactInfo contact)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _repo.UpdateAsync(id, contact);
+            if (!result)
+                return NotFound("Contact not found");
+
+            return Ok("Updated successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _repo.DeleteAsync(id);
+            if (!result)
+                return NotFound("Contact not found");
+
+            return Ok("Deleted successfully");
+        }
+    }
+}
---
+++ week8/Day3/Models/ContactInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication12.Models
+{
+    public class ContactInfo
+    {
+        public int ContactId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string EmailId { get; set; }
+
+        [Required]
+        public long MobileNo { get; set; }
+
+        [Required]
+        public string Designation { get; set; }
+
+        [Required]
+        public int CompanyId { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+    }
+}
