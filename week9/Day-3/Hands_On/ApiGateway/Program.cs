9d2d0305c9221a04d3a8378a7a68d4e4ffac09f9
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-04-15 22:47:38 +0530

    Add files via upload

---
+++ week9/Day-3/Hands_On/ApiGateway/Program.cs
@@ -0,0 +1,21 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace ApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+            builder.Services.AddOcelot();
+
+
+            var app = builder.Build();
+            app.UseOcelot();
+
+            app.Run();
+        }
+    }
+}
---
+++ week9/Day-3/Session_Example/ConsumerService/Services/IProductApiService.cs
@@ -0,0 +1,13 @@
+﻿using DemoService.Models;
+
+namespace DemoService.Services
+{
+    public interface IProductApiService
+    {
+        Task<Product> GetProductId(int id);
+        Task<List<Product>> GetProducts();
+        Task<Product> AddProduct(Product product);
+        Task<Product> UpdateProduct(int id, Product product);
+        Task<string> DeleteProduct(int id);
+    }
+}
