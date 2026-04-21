5913bc15797a12514acf26cbfc3df81d9256b342
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-04-21 18:55:40 +0530

    Delete week9 directory

--- week9/Day-1/HandsOn_2/Models/ErrorResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication14.Models
-{
-    public class ErrorResponse
-    {
-        public string Message { get; set; }
-        public int StatusCode { get; set; }
-        public DateTime Timestamp { get; set; }
-    }
-}
--- week9/Day-2/Caching/Models/Contact.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication16.Models
-{
-    public class Contact
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
