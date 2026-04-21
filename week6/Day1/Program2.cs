c722270d797b56b7dcf2f98a3e490e0fa0ed0e77
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-04-21 18:55:08 +0530

    Delete week6 directory

--- week6/Day1/Program2.cs
+++
@@ -1,16 +0,0 @@
-﻿using System;using System.IO;using System.Threading.Tasks;using System.Net.Http;namespace ConsoleApp8{    class Program    {        static void Main(string[] args)        {
-            Console.Write("Enter Product Name: ");
-            string productName = Console.ReadLine();
-
-            Console.Write("Enter Price: ");
-            double price = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Enter Discount (%): ");
-            double discount = Convert.ToDouble(Console.ReadLine());
-
-            double finalPrice = price - (price * discount / 100);
-
-            Console.WriteLine("Product: " + productName);
-            Console.WriteLine("Final Price: " + finalPrice);        }
-    }
-}
--- week6/Day1/Program3.cs
+++
@@ -1,34 +0,0 @@
-﻿using System;using System.IO;using System.Threading.Tasks;using System.Net.Http;namespace ConsoleApp8{    class Program    {        static async Task Main()
-        {
-            Console.WriteLine("Starting report generation...\n");
-            Task t1 = GenerateSalesReport();
-            Task t2 = GenerateInventoryReport();
-            Task t3 = GenerateCustomerReport();
-
-            await Task.WhenAll(t1, t2, t3);
-
-            Console.WriteLine("\nAll reports generated successfully.");
-        }
-
-        static async Task GenerateSalesReport()
-        {
-            Console.WriteLine("Sales Report Started...");
-            await Task.Delay(2000); 
-            Console.WriteLine("Sales Report Completed.");
-        }
-
-        static async Task GenerateInventoryReport()
-        {
-            Console.WriteLine("Inventory Report Started...");
-            await Task.Delay(3000);
-            Console.WriteLine("Inventory Report Completed.");
-        }
-
-        static async Task GenerateCustomerReport()
-        {
-            Console.WriteLine("Customer Report Started...");
-            await Task.Delay(1500);
-            Console.WriteLine("Customer Report Completed.");
-        }
-    }
-}
