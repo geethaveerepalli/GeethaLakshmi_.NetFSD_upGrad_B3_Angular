910e32ceff5228bb7bbb76151c005475df5f4d2f
geethaveerepalli <106485814+geethaveerepalli@users.noreply.github.com>
2026-04-22 14:43:29 +0530

    Add files via upload

---
+++ week-10/Day-1/Day-3/Demo1.js
@@ -0,0 +1,9 @@
+"use strict";
+let uname = "Scott";
+let age = 25;
+let isValidUser = true;
+console.log(`User Name :   ${uname}`);
+console.log(`User Age : ${age}`);
+console.log(`Is Valid User : ${isValidUser}`);
+console.log("---------------------------------");
+console.log(`${uname}, ${age}, ${isValidUser}`);
